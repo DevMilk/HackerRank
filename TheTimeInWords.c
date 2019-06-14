b212d7b87436a6d7f6126817faf86f4b8a94e4e2
Uğur Keskin <41523809+DevMilk@users.noreply.github.com>
2019-06-14 22:46:49 +0300

    Create TheTimeInWords.c

---
+++ TheTimeInWords.c
@@ -0,0 +1,160 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+// Complete the timeInWords function below.
+
+// Please either make the string static or allocate on the heap. For example,
+// static char str[] = "hello world";
+// return str;
+//
+// OR
+//
+// char* str = "hello world";
+// return str;
+//
+char* word(int num){
+    char* a=malloc(40);
+    switch(num){
+        case 1:
+            return "one";
+            break;
+        case 2:
+            return "two";
+            break;
+        case 3:
+            return "three";
+            break;
+        case 4:
+            return "four";
+            break;
+        case 5:
+            return "five";
+            break;
+        case 6:
+            return "six";
+            break;
+        case 7:
+            return "seven";
+            break;
+        case 8:
+            return "eight";
+            break;
+        case 9:
+            return "nine";
+            break;
+        case 10:
+            return "ten";
+            break;
+        case 11:
+            return "eleven";
+            break;
+        case 12:
+            return "twelve";
+            break;
+        case 13:
+            return "thirteen";
+            break;
+        case 14:
+            return "fourteen";
+            break;
+        case 16:
+            return "sixteen";
+            break;
+        case 17:
+            return "seventeen";
+            break;
+        case 18:
+            return "eighteen";
+            break;
+        case 19:
+            return "nineteen";
+            break;
+        case 20:
+            return "twenty";
+            break;
+        default:
+            sprintf(a,"twenty %s",word(num%10));
+            return a;
+            break;
+    }
+}
+char* timeInWords(int h, int m) {
+    char* output= malloc(sizeof(char)*40);
+    if(m==0)
+        sprintf(output,"%s o' clock",word(h));
+    else{
+        sprintf(output,"%s",(m==15 || m==45) ? "quarter": m==30 ? "half": (m<=30 ? word(m): word(60-m)));
+        if(m!=15 && m!=30 && m!=45)
+            strcat(output,m==1 ? " minute": " minutes");
+        
+        strcat(output,m<=30 ? " past ": " to ");
+        strcat(output,m<=30 ? word(h): word(h+1));
+    }
+    return output;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* h_endptr;
+    char* h_str = readline();
+    int h = strtol(h_str, &h_endptr, 10);
+
+    if (h_endptr == h_str || *h_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    char* m_endptr;
+    char* m_str = readline();
+    int m = strtol(m_str, &m_endptr, 10);
+
+    if (m_endptr == m_str || *m_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    char* result = timeInWords(h, m);
+
+    fprintf(fptr, "%s\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
